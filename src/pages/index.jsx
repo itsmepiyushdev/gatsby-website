1bf99c0b058bc2bb1f3ad992bcef140c7703e613
itsmepiyushdev <piyush.dhage@gmail.com>
2023-04-07 06:51:06 +0530

    Added Tailwind CSS

---
+++ src/pages/index.jsx
@@ -0,0 +1,14 @@
+import * as React from "react"
+const IndexPage = () => {
+  return (
+    <>
+      <h1 className="text-3xl font-bold underline text-fuchsia-400">
+        Hello world! Tailwind is Installed
+      </h1>
+    </>
+  )
+}
+
+export default IndexPage
+
+export const Head = () => <title>Home Page</title>
